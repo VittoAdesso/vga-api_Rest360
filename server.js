68a76b813f9ed3259cda58633fe22c38fd8f8de7
Vittorio Adesso <adessovittorio8@gmail.com>
2022-03-06 22:24:21 +0100

    SET FOLDER TO SERVER STATIC ASSETS

--- server.js
+++ server.js
@@ -4,6 +4,19 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+// SET FOLDER TO SERVER STATIC ASSETS
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization, username")
+    //and remove cacheing so we get the most recent comments
+    res.setHeader('Cache-Control', 'no-cache');
+    next();
+  });
+
 app.use(express.urlencoded({ extended : true }));
 app.use(express.json());
 

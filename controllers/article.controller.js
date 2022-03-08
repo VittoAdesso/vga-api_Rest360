9e017eb6eeb80637fdc9ac2bfca3a37d93bb4d29
Vittorio Adesso <adessovittorio8@gmail.com>
2022-03-08 23:22:46 +0100

    Implemented add controller article try & catch to control error

--- controllers/article.controller.js
+++ controllers/article.controller.js
@@ -8,16 +8,41 @@ exports.findAll = async (req, res) => {
     res.send(articles);
 }
 
+// exports.findOne = async (req, res) => {
+
+//     const { id } = req.params;
+    
+//     const articles = await Article.findOne({
+//         where: {
+//             id : id,
+//         },
+//     });
+
+//     res.send(articles);
+// }
+
+
 exports.findOne = async (req, res) => {
 
     const { id } = req.params;
-    
-    const articles = await Article.findOne({
-        where: {
-            id : id,
-        },
-    });
 
-    res.send(articles);
+    try {
+        const articles = await Article.findOne({
+            where: {
+                id : id,
+            },
+        });
+        
+        res.send(articles);
+
+    } catch (error) {
+        res.status(500).send({
+            message: "Error retrieving Article with id=" + id
+        });
+    
+    }
 }
+
+
+
 

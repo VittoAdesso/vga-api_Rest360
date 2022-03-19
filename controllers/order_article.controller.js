5f21866d8234f5096389192a567bce1ff7e65a56
Vittorio Adesso <adessovittorio8@gmail.com>
2022-03-19 20:02:03 +0100

    add new method findAll to orderArticle

--- controllers/order_article.controller.js
+++ controllers/order_article.controller.js
@@ -1,20 +1,40 @@
 const db = require('../models');
 const OrderArticle = db.orderArticle;
 
+//FINDING ALL
 exports.findAll = async (req, res) => {
 
-    const orderArticle = await OrderArticle.findAll();
+    const orderArticles = await OrderArticle.findAll();
 
-    res.send(orderArticle);
+    res.send(orderArticles);
 }
 
 // findOne un id, controlando errores en case que no encuentre uno existente 
 exports.findOne = async (req, res) => {
 
-    const idOrder = req.params.idOrder; 
-  
+    const id = req.params.id; 
     try { 
-      const order = await OrderArticle.findOne({
+      const orderId = await OrderArticle.findOne({
+        where: {
+                id : id,
+        },
+      });
+  
+    if (orderId) { 
+      return res.status(200).json(orderId);} 
+        else { 
+        return res.status(404).json('No order article found by this id'); } 
+        } 
+  
+    catch (err) { return res.status(500).json(err); } 
+};
+
+  // FINDall idOrder, el cuál no es Pk, ni first column in msql, is another one , controlando errores en case que no encuentre uno existente (ojo, que tengan el mismo idOrder)
+  exports.findAll = async (req, res) => {
+
+    const idOrder = req.params.idOrder; 
+    try { 
+      const order = await OrderArticle.findAll({
         where: {
                 idOrder : idOrder,
         },
@@ -22,14 +42,9 @@ exports.findOne = async (req, res) => {
   
     if (order) { 
       return res.status(200).json(order);} 
-  
       else { 
         return res.status(404).json('No order article found by this idOrder'); } 
-    
     } 
-  
-    catch (err) { return res.status(500).json(err); } 
+  catch (err) { return res.status(500).json(err); } 
   
   };
-
-  // findOne un idOrder, el cuál no es Pk, ni first column in msql, is another one , controlando errores en case que no encuentre uno existente 
--- routes/order_article.routes.js
+++ routes/order_article.routes.js
@@ -2,9 +2,14 @@ module.exports = app => {
     const orderArticle = require('../controllers/order_article.controller');
     const router = require('express').Router();
 
+    // TO FIND AND SHOW ALL 
     router.get('/', orderArticle.findAll);
-    // remember you want to find an other article witch one is not an id 
-    router.get('/:idOrder', orderArticle.findOne);
 
+    // TO FIND ONE ORDER WITH ID SPECIFIC
+    router.get('/:id', orderArticle.findOne);
+
+// remember you want to find an other article witch one is not an id    
+    router.get('/order/:idOrder', orderArticle.findAll);
+// GRAL ROUTE
     app.use('/vga/orderArticles', router);
 }
